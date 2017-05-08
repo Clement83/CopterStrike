0de9180d9619c7b319b76854fdf911f6f410bdfc
Clement <clement@quintard.me>
2017-05-08 21:21:09 +0200

    avion de chasse

--- missions/mer/planes.ino
+++ missions/mer/planes.ino
@@ -1,21 +1,53 @@
+const byte avionChasse[] PROGMEM = {
+  16,10,
+  0x00,0x3E,
+  0x00,0x78,
+  0x0F,0x84,
+  0x1F,0xFC,
+  0x31,0xF8,
+  0x77,0xC8,
+  0xBF,0xB8,
+  0xFF,0x7C,
+  0x0F,0xFE,
+  0x00,0x3F,
+};
+
+#define NB_PLANES 3
+typedef struct{
+  int x;
+  int y;
+} Planes;
+Planes planes[NB_PLANES];
 
 void initPlanes()
 {
-
+    planes[0].x = 680;
+    planes[0].y = 240;
+    planes[1].x = 670;
+    planes[1].y = 250;
+    planes[2].x = 680;
+    planes[2].y = 260;
 }
 
 void updatePlanes()
 {
 
+  for(byte i=0;i<NB_PLANES;i++){
+    planes[i].x -=3;
+
+    if(planes[i].x<16) planes[i].x += 680; 
+  } 
 }
 
 
 void drawPlanes()
 {
-    byte coordx= PORTEAVION_XWORLD - player.x_world;
-    byte coordy= PORTEAVION_YWORLD - player.y_world;
-
-    if( player.x_world>154 && player.x_world<315 && player.y_world>230 && player.y_world<334){
-      gb.display.drawBitmap(coordx,coordy,porteAvion2);
-    }
+  for(byte i=0;i<NB_PLANES;i++){
+       coordx=fnctn_lndscapeXpos(planes[i].x );
+       coordy=fnctn_lndscapeYpos(planes[i].y);
+         
+      if(coordx<SCREENWIDTH  && coordx>0-bkgrnd[i].width   && coordy<SCREENHEIGHT    && coordy>0-bkgrnd[i].height){
+         gb.display.drawBitmap(coordx, coordy, avionChasse);
+      }
+  }
 }
